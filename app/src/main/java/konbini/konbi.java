1fe2cd7855b2c74bd440d931f3301ccde793990d
leo <131971014+dayview@users.noreply.github.com>
2025-10-23 11:41:46 +0800

    chore: delete konbi.java

--- app/src/main/java/konbini/konbi.java
+++
@@ -1,9 +0,0 @@
-package konbini;
-
-import konbini.controllers.StoreController;
-import konbini.models.*;
-import konbini.services.LoginSystem;
-import konbini.views.StoreView;
-
-public class konbi {
-}
